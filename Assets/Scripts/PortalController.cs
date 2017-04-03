01f2263346233fbd12f9de15b952d36ddf534489
jpramante <jpramante@gmail.com>
2017-04-02 22:36:41 -0300

    Fase 1 montada

--- Assets/Scripts/PortalController.cs
+++ Assets/Scripts/PortalController.cs
@@ -8,18 +8,35 @@ public class PortalController : MonoBehaviour {
 	GameObject PortalExit;
 	GameObject PortalEnter;
 
+	int SizePortal;
+	int posi;
+
+	public GameObject[] PortaisEnter =  new GameObject[(1)];
+	public GameObject[] PortaisExit = new GameObject[(1)];
+
 	void Start ()
 	{
+		SizePortal = PortaisEnter.Length;
 		//adicionar as tags "PortalExit" e "PortalEnter" dos portais
 		PortalExit = GameObject.FindGameObjectWithTag ("PortalExit");
 		PortalEnter = GameObject.FindGameObjectWithTag ("PortalEnter");
 	}
+	void Update()
+	{
+		
+	}
 	void OnTriggerEnter2D (Collider2D col)
 	{
-		if (col.gameObject.tag == "PortalEnter") 
+		for(posi = 0; posi <= SizePortal - 1; posi++)//inicia o for para rodar a array para localizar qual o portal que esta colidindo
 		{
-			Vector2 PortalPosition = PortalExit.transform.position;
-			gameObject.transform.position = PortalPosition;
+			if (PortaisEnter[posi] == col.gameObject)//verifica se o portal que foi colidido pelo player é igual ao do loop
+			{
+				if (col.gameObject.tag == "PortalEnter") {
+					Vector2 PortalPosition = PortaisExit [posi].transform.position;
+					gameObject.transform.position = PortalPosition;
+				}
+			}
+
 		}
 		if (col.gameObject.tag == "EndFase") 
 		{
