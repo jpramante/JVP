5786f1d7a49c5a58e27f99a68dc5c3d1f8d0ef39
jpramante <jpramante@gmail.com>
2017-05-31 13:53:20 -0300

    Consertando colliders das plataformas

---
+++ Assets/Scripts/TimedPlatform.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimedPlatform : MonoBehaviour {
+
+	public GameObject timedPlatformScript;
+    private bool collisionDetected;
+    private float platTimer = 0f;
+
+	void Start () 
+	{
+        collisionDetected = false;
+		timedPlatformScript.SetActive(false);
+	}
+    void OnCollisionEnter2D(Collision2D col) 
+	{
+        collisionDetected = true;
+	}
+
+    void Update()
+    {
+        if (collisionDetected)
+        {
+            timedPlatformScript.SetActive(true);
+            platTimer += Time.deltaTime;
+
+        }
+    }
+
+
+}
